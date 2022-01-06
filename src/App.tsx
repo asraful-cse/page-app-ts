3111e98c22104890ecc4f58147da802f3eecf2cb
asraful196 <asraful.01110@gmail.com>
2022-01-06 12:17:12 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+const Home = lazy(() => import('./components/pages/Home'));
+const Details = lazy(() => import('./components/pages/Details'));
+
+const App: React.FC = () => {
+  return (
+    <div className="App" data-testid="app">
+      <Suspense fallback={<p>Loading...</p>}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/details" component={Details} />
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Details.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {useLocation} from "react-router-dom";
+import {InitPost} from "./Home";
+
+const Details: React.FC = () => {
+
+    const {state} = useLocation();
+
+    const post = state as InitPost;
+
+    return (
+        <div data-testid="details">
+            <pre>
+                {
+                    JSON.stringify(post, null, 2)
+                }
+            </pre>
+        </div>
+    )
+}
+
+export default Details;
